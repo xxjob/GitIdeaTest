37fdc495a4ea866a94c801f4da0cb987e6a64a8a
Administrator <420199162@qq.com>
2021-04-12 17:29:11 +0800

    Initial commit

---
+++ module1/src/main/java/com/xx/test/Test2.java
@@ -0,0 +1,14 @@
+package com.xx.test;
+
+/**
+ * @Description
+ * @Author lxx
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2021/4/12
+ */
+public class Test2 {
+    public static void main(String[] args) {
+
+    }
+}
